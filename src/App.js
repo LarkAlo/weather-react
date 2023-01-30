528bc647ab094894a41733f3d2460da1c50470af
LarkAlo <118227350+LarkAlo@users.noreply.github.com>
2023-01-30 13:00:59 -0700

    Added footer
    added footer with link to respository

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./styles.css";
 import Search from "./Search";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/LarkAlo/weather-react" target="_blank">
+        Open-Source
+      </a>
+      by Kimberly Cossio
+    </footer>
+  );
+}
